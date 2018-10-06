f69c8c41d67983629f716f6c1cb8b58e42f41801
yerbamate994 <yerbamate994@gmail.com>
2018-10-06 15:29:23 +0900

    잠금화면 풀림 상태 리시버로 인식, 구현 성공

--- app/src/main/java/company/myproject/www/bepatient/ScreenCountService.java
+++ app/src/main/java/company/myproject/www/bepatient/ScreenCountService.java
@@ -3,7 +3,9 @@ package company.myproject.www.bepatient;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -13,7 +15,9 @@ import static android.app.PendingIntent.FLAG_CANCEL_CURRENT;
 
 public class ScreenCountService extends Service {
 
-    public static final String TAG ="ScreenCountService";
+    public static final String TAG = "ScreenCountService";
+    private BroadcastReceiver mReceiver;
+    private IntentFilter mIntentFilter;
 
     // 서비스가 시작되면 onStartCommand가 호출 됨.
     @Override
@@ -21,6 +25,10 @@ public class ScreenCountService extends Service {
 
         startForeground(1, setNotification()); // TaskKiller에 서비스가 죽지 않도록 하기 위하여
         // + 노티피케이션 실행
+
+        mReceiver = new LockScreenStateReceiver(); // 화면켜짐 액션 받을 리시버 객체 생성
+        mIntentFilter = new IntentFilter(Intent.ACTION_USER_PRESENT); // 화면 켜짐(잠금화면 풀린 상태) 액션 필터 등록
+        registerReceiver(mReceiver, mIntentFilter); // 브로드캐스트 리시버 등록
 
         return super.onStartCommand(intent, flags, startId);
     }
@@ -30,6 +38,7 @@ public class ScreenCountService extends Service {
         super.onDestroy();
 
         stopForeground(true); // Foreground 죽이고, 노티피케이션도 죽임.
+        unregisterReceiver(mReceiver); // 리시버 등록 해제
     }
 
     @Nullable
@@ -43,7 +52,9 @@ public class ScreenCountService extends Service {
 
         // 노티피케이션 터치시 액티비티 실행을 위한 인텐트 설정
         Intent mIntent = new Intent(this, MainActivity.class);
-        mIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); // 액티비티 중복 실행을 막기 위한 플래그
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // 액티비티 중복 실행을 막기 위한 플래그 설정
 
         // 설정된 인텐트를 펜딩인텐트에 set
         PendingIntent mPendingIntent =
